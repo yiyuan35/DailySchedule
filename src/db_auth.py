f3fa0a9be654155de6a67736d5f7cf7d9f8f9e58
yuenachen93 <yuenachen93@gmail.com>
2018-11-28 18:11:22 +0000

    Add login

---
+++ src/db_auth.py
@@ -0,0 +1,94 @@
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+import datetime
+import hashlib
+import os
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    # User information
+    email = db.Column(db.String, nullable=False, unique=True)
+    # password_digest = db.Column(db.String, nullable=False)
+    # Session information
+    sub = db.Column(db.String, nullable = False)
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+    # cascade='delete' => when an user is deleted, delete all event
+    events = db.relationship('Event', cascade='delete')
+
+    def __init__(self, **kwargs):
+        self.email = kwargs.get('email')
+        # takes input for password nad encodes
+        # self.password_digest = bcrypt.hashpw(kwargs.get('password').encode('utf8'),
+        #                                     bcrypt.gensalt(rounds=13))
+        self.sub = kwargs.get('sub')
+        self.renew_session()
+
+    # Used to randomly generate session/update tokens
+    def _urlsafe_base_64(self):
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+        # helper function to generate tokens
+
+    # Generates new tokens, and resets expiration time
+    def renew_session(self):
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + \
+                                datetime.timedelta(days=1) #can motify the expiration date
+        self.update_token = self._urlsafe_base_64()
+
+    # def verify_password(self, password):
+    #     # check password given with the encypted password
+    #     return bcrypt.checkpw(password.encode('utf8'),
+    #                           self.password_digest)
+
+    def verify_sub(self, sub):
+        return sub = self.sub
+
+    # Checks if session token is valid and hasn't expired
+    def verify_session_token(self, session_token):
+        return session_token == self.session_token and \
+            datetime.datetime.now() < self.session_expiration   #check not expired
+
+    def verify_update_token(self, update_token):
+        return update_token == self.update_token
+
+
+class Event(db.Model):
+    __tablename__ = 'event' 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    startTime = db.Column(db.DateTime, nullable=False)
+    endTime = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String, nullable=True)
+    reminder = db.Column(db.DateTime, nullable=True)
+    priority = db.Column(db.Integer, nullable=True)
+    # task id to the task it belong to
+    # task.id => table name.field name
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.title = kwargs.get('title', '')
+        # if a title was given in parameter, then use the value or initial value which is ''
+        self.description = kwargs.get('description', '')
+        self.startTime = kwargs.get('startTime', '')
+        self.endTime = kwargs.get('endTime', '')
+        self.location = kwargs.get('location', '')
+        self.reminder = kwargs.get('reminder', '')
+        self.priority = kwargs.get('priority', 1)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'startTime': self.startTime,
+            'endTime': self.endTime,
+            'location': self.location,
+            'reminder': self.reminder,
+            'priority': self.priority
+        }
